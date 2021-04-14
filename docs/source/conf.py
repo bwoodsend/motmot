c6453f439b9d02c095e7c9a32c6f594341ddbb60
bwoodsend <bwoodsend@gmail.com>
2021-04-14 10:44:16 +0100

    Temporarily hard-code intersphinx paths to hoatzin and rockhopper.
    To be replaced with real urls once they are on readthedocs.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -92,6 +92,8 @@ with open("rst_prolog.txt") as f:
 intersphinx_mapping = {
     'python': ('http://docs.python.org/3', None),
     'numpy': ('https://numpy.org/doc/stable/', None),
+    'hoatzin': ('/e/notebooks/hoatzin/docs/build/html', None),
+    'rockhopper': ('/e/notebooks/rockhopper/docs/build/html', None),
 }
 
 # Autodoc defaults.
